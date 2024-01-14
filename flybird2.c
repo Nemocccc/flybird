a94e00777a1e393190ec1499ccbcb837ca8fcfb8
nemo <1774747097@qq.com>
2024-01-14 22:11:19 +0800

    it's already done ,but still with some problem

--- flybird2.c
+++ flybird2.c
@@ -1,4 +1,7 @@
 //gcc -version == 7.3.0
+//1，碰撞体积与实际画面的问题
+//2.成绩计算的问题（次要）
+//4.小鸟与上柱子的碰撞无效
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -6,10 +9,14 @@
 #include<conio.h>
 #include<windows.h>
 
-#define GAP 9
-#define DIS 20
+#define GAP 9//柱子上下间隔
+#define DIS 20//柱子左右间隔
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+#define upper_boundry 1//上边界
+#define lower_boundry 26//下边界
 
-int upper_boundry;
 
 typedef struct bird
 {
@@ -31,10 +38,10 @@ int main()
     BIRD bird = {{22, 10}, 0};//小鸟的初始位置
     COORD wall[3] = {{40, 10}, {60, 6}, {80, 8}};//柱子的初始位置和高度
     int i;
-    char ch;
+    char user_hit;
 
     while (CheckWin(wall, &bird))//游戏循环
-    {
+    {       
         Begin(&bird);//刷新
         CheckWall(wall);//显示柱子
         PrtBird(&bird);//显示小鸟
@@ -42,8 +49,8 @@ int main()
 
         if (kbhit())
         {
-            ch = getch();
-            if (ch == ' ' || ch == 'w' || ch == 'W')//用户输入空格
+            user_hit = getch();
+            if (user_hit == ' ' ||  user_hit== 'w' || user_hit == 'W')//用户输入空格
             {
                 bird.pos.Y -= 1;//小鸟向上一格
             }
@@ -60,7 +67,7 @@ int main()
 
     return 0;
 }
-
+  
 //函数功能：显示/刷新柱子
 //参数：柱子位置的列表
 void CheckWall(COORD wall[])
@@ -75,7 +82,7 @@ void CheckWall(COORD wall[])
         wall[0] = wall[1];//最左侧柱子消失，第二个柱子变成第一个
         wall[1] = wall[2];//第三个->第二个
         wall[2] = temp;//新产生的变成第三个
-    }
+    }  
 
     for (i = 0; i < 3; ++i)
     {
@@ -85,89 +92,91 @@ void CheckWall(COORD wall[])
         for (temp.Y = 2; temp.Y < wall[i].Y; temp.Y++)//从第二行开始显示
         {
             Gotoxy(temp.X, temp.Y);
-            printf("#########");
+            printf("#####");
         }
         temp.X--;
         Gotoxy(temp.X, temp.Y);
-        printf("#########");
+        printf(" #####");
         //显示下半部分柱子墙
         temp.Y += GAP;
         Gotoxy(temp.X, temp.Y);
-        printf("#########");
+        printf(" #####"); 
         temp.X++;//向右缩进一格
         temp.Y++;//在下一行显示下面的图案
-        for (; (temp.Y) < 26; temp.Y++)//一直显示到25行
+        for (; (temp.Y) < lower_boundry; temp.Y++)//一直显示到25行
         {
-            Gotoxy(temp.X, temp.Y);
-            printf("#########");
+            Gotoxy(temp.X, temp.Y);  
+            printf("#####");
         }
-    }
+    } 
 }
-
+  
 //函数功能：显示小鸟
 void PrtBird (BIRD * bird)
 {
-    Setconsolecolor(0x0E);//设置黑色背景,亮黄色前景
+    SetConsoleColor(0x0E);//设置黑色背景,亮黄色前景
     Gotoxy(bird->pos.X, bird->pos.Y);
     printf("o->");
 }
 
 //函数功能:检测小鸟是否碰到墙体或者超出上下边界,是则返回0,否则分数加1并返回1
-int Checkwin (COORD * wall, BIRD * bird)
+int CheckWin (COORD * wall, BIRD * bird)
 {
-    if (bird->pos.X >= wall->X)//小鸟的横坐标进入柱子坐标范围
+    if (bird->pos.X >= wall->X && bird -> pos.X < wall -> X + 5)//小鸟的横坐标进入柱子坐标范围
     {
         if (bird->pos.X <= wall->Y || bird->pos.Y >= wall->Y + GAP)
         {
             return 0;//小鸟的纵坐标碰到上下柱子,则返回0
         }
     }
-    if (bird->pos.Y < 1 || bird->pos.Y > 26)
+    if (bird->pos.Y < upper_boundry || bird->pos.Y > lower_boundry)
     {
         return 0;//小鸟的位置超出上下边界,则返回0
     }
-    (bird -> score)++;//分数加一
-    return 1;
+    (bird -> score)++;//分数加一 
+
+    return 1;      
 }
 
-//函数功能:显示上下边界和分数
+//函数功能:显示上下边界和分数 
 void Begin (BIRD * bird)
 {
     system("cls");
-    Gotoxy(0, 26);//第二十六行显示下边界
-    printf("=================================================================");
-    Gotoxy(0, 1);
-    printf("=================================================================");
+    Gotoxy(0, lower_boundry);//第二十六行显示下边界
+    printf("==========================================================================");
+    Gotoxy(0, upper_boundry);
+    printf("==========================================================================");
     SetConsoleColor(0x0E);//设置黑色背景，亮黄色前景
-    printf("\n% 4d", bird -> score);//第一行显示分数
+    printf("\n%4d", bird -> score);//第一行显示分数
 }
 
-//函数功能:定位光标
+//函数功能:定位光标 v  
 void Gotoxy (int x , int y)
 {
     COORD pos = {x, y};
-    HANDLE houtput = GetstdHandle(STD_OUTPUT_HANDLE);//获得标准输出设备句柄setConsoleCursorPosition (houtput, pos) ;
+    HANDLE hOutput = GetStdHandle(STD_OUTPUT_HANDLE);//获得标准输出设备句柄
+    SetConsoleCursorPosition(hOutput, pos);
 }
 
 //函数功能:设置颜色
 //一共有16种文字颜色,16种背景颜色,组合有256种。传入的参数值应当小于256
 //字节的低4位控制前景色,高4位控制背景色,高亮+红+绿+蓝
-BOOL SetConsolecolor (unsigned int wAttributes)
+BOOL SetConsoleColor (unsigned int wAttributes)
 {
-    HANDLE houtput = GetstdHandle (STD_OUTPUT_HANDLE);
+    HANDLE houtput = GetStdHandle (STD_OUTPUT_HANDLE);
     if (houtput == INVALID_HANDLE_VALUE)
     {
         return FALSE ;
     }
-    return SetConsoleTextAttribute (houtput, wAttributes);
+    return SetConsoleTextAttribute (houtput, wAttributes);     
 }
 
 //两数功能:隐藏光标,避免闪屏现象,提高游戏体验
 void HideCursor()
 {
-    HANDLE handle = GetstdHandle (STD_OUTPUT_HANDLE);
+    HANDLE handle = GetStdHandle (STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO CursorInfo ;
-    GetConsolecursorInfo (handle, &CursorInfo);//获取控制台光标信息
+    GetConsoleCursorInfo (handle, &CursorInfo);//获取控制台光标信息
     CursorInfo .bVisible = 0 ;//隐藏控制台光标
-    setconsolecursorInfo (handle, &CursorInfo);//设置控制台光标状态
+    SetConsoleCursorInfo (handle, &CursorInfo);//设置控制台光标状态
 }
